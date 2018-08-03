e33a40607cd04ceb6958784bf3f1da6e8a9a788e
zixuan.guo <616447315@qq.com>
2018-08-03 12:24:49 +0800

    upload snake.py add self-detection

--- snake.py
+++ snake.py
@@ -1,11 +1,11 @@
 import turtle
 import random
 
+
 class Snake(turtle.Turtle):
     number = 0
     bodyPos = []
     torwarding = 0
-    over = False
 
     def __init__(self, color):
         turtle.Turtle.__init__(self)
@@ -22,83 +22,68 @@ class Snake(turtle.Turtle):
         self.move()
 
     def keyListening(self):
-        turtle.onkeypress(self.u, "Up")
-        turtle.onkeypress(self.l, "Left")
-        turtle.onkeypress(self.r, "Right")
-        turtle.onkeypress(self.d, "Down")
+        turtle.onkeypress(self.up, "Up")
+        turtle.onkeypress(self.left, "Left")
+        turtle.onkeypress(self.right, "Right")
+        turtle.onkeypress(self.down, "Down")
         turtle.listen()
 
-    def u(self):
+    def up(self):
         if self.torwarding == 270:
             pass
         else:
             self.torwarding = 90
 
-    def d(self):
+    def down(self):
         if self.torwarding == 90:
             pass
         else:
             self.torwarding = 270
 
-    def l(self):
+    def left(self):
         if self.torwarding == 0:
             pass
         else:
             self.torwarding = 180
 
-    def r(self):
+    def right(self):
         if self.torwarding == 180:
             pass
         else:
             self.torwarding = 0
 
-    def isOver(self):
-        x = self.xcor()
-        y = self.ycor()
-        if(x < -220 or x > 220 or
-           y > 220 or y < -220):
-            self.overScene()
-
-    def overScene(self):
-        self.over = True
-        turtle.clear()
-        self.clear()
-        self.setposition(0, 0)
-        turtle.setposition(0, 0)
-        turtle.write("Game over", align="center",
-                     font=("Arial", 20, "bold"))
-
     def move(self):
-
         self.up()
+        self.color = 'red'
         self.setheading(self.torwarding)
-        self.stamp()
         self.fd(10)
+        self.stamp()
         x = round(self.xcor())
         y = round(self.ycor())
         if self.number > self.length:
             self.clearstamps(1)
             self.bodyPos.append([x, y])
-            self.bodyPos.pop(-1)
+            self.bodyPos.pop(0)
         else:
             self.bodyPos.append([x, y])
             self.number += 1
-        self.isOver()
         turtle.ontimer(self.move, 80)
 
 
 class bonus(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
-        x = random.randrange(-10, 10, 1)
-        y = random.randrange(-10, 10, 1)
         self.hideturtle()
         self.shape("circle")
         self.shapesize(0.5)
         self.color("red")
         self.penup()
-        self.goto(x*20, y*20)
-        self.stamp()
+
+    def gen(self):
+        x = random.randrange(-200, 200, 10)
+        y = random.randrange(-200, 200, 10)
+        self.goto(x, y)
+        return [self.stamp(), x, y]
 
 
 class border:
@@ -121,69 +106,86 @@ class border:
 
 class game:
     state = 'null'
+    bonusList = []
 
     def welcome(self):
         self.state = "welcome"
         turtle.clear()
         turtle.hideturtle()
         turtle.up()
-        turtle.pensize(2)
-        colors = ["red", "yellow", "purple", "blue"]
+        turtle.pensize(1)
+        colors = ["#FFAA00", "#BF8F30", "#A66F00", "#FFBF40", "#FFD073", "#3914AF",
+                  "#3914AF", "#412C84", "#200772", "#6A48D7", "#876ED7", "#009999",
+                  "#1D7373", "#412C84", "#006363", "#5CCCCC", "#33CCCC"]
         turtle.setposition(-150, 20)
         turtle.tracer(False)
         turtle.pd()
-        for x in range(40):
-            turtle.forward(2*x)
-            turtle.color(colors[x % 4])
-            turtle.left(91)
-        turtle.setposition(75, 0)
-        turtle.write("Welcome to Snake Game", align="center",
+        for x in range(60):
+            turtle.fd(x/4)
+            turtle.dot(15)
+
+            turtle.color(colors[random.randrange(0, 15, 1)])
+            turtle.left(15)
+
+        turtle.setposition(75, -30)
+        turtle.write("Welcome to Snake Game\n\nPress G to begin", align="center",
                      font=("Arial", 20, "bold"))
-        turtle.setposition(15, -35)
-        turtle.write("Press G to begin", align="center",
-                     font=("Arial", 20, "normal"))
         self.keyListening()
 
     def start(self):
         turtle.onkey(None, key="g")
-        self.state = "stated"
-        try:
-            self.snake.clear()
-            turtle.clear()
-            self.border = border()
-            self.snake = Snake('blue')
-            self.border.draw()
-            self.snake.draw()
-            self.bonus = bonus()
+        self.state = "started"
+        turtle.clear()
+        self.border = border()
+        self.bonus = bonus()
+        self.snake = Snake('blue')
+        self.border.draw()
+        self.snake.draw()
+        self.isOver()
+        self.genBonus()
+        self.eating()
 
-            self.genBonus()
-            self.eating()
+    def overScene(self):
+        self.state = 'over'
+        turtle.clear()
+        self.snake.clear()
+        self.bonus.clear()
+        self.bonusList.clear()
+        turtle.setposition(0, 0)
+        turtle.write("Game over\nPress G to restart", align="center",
+                     font=("Arial", 20, "bold"))
+        self.keyListening()
 
-        except:
-            self.border = border()
-            self.snake = Snake('blue')
-            self.border.draw()
-            self.snake.draw()
-            self.bonus = bonus()
-
-            self.genBonus()
-            self.eating()
+    def isOver(self):
+        x = self.snake.xcor()
+        y = self.snake.ycor()
+        if(x < -220 or x > 220 or
+           y > 220 or y < -220):
+            self.overScene()
+        for i in self.snake.bodyPos:
+            if self.snake.bodyPos.count(i) > 1:
+                self.overScene()
+        if self.state == 'started':
+            turtle.ontimer(self.isOver, 20)
 
     def eating(self):
-        if (self.bonus != None and self.snake.xcor() < self.bonus.xcor()+0.5 and self.snake.xcor() > self.bonus.xcor()-0.5 and self.snake.ycor() < self.bonus.ycor() + 0.5 and self.snake.ycor() > self.bonus.ycor() - 0.5):
+        # print(self.bonusList[0])
+        # print([self.snake.xcor(),self.snake.ycor()])
+        if (len(self.bonusList) > 0 and self.snake.xcor() < self.bonusList[0][1]+1 and self.snake.xcor() > self.bonusList[0][1] - 1 and self.snake.ycor() < self.bonusList[0][2] + 1 and self.snake.ycor() > self.bonusList[0][2] - 1):
             self.snake.length += 1
-            self.bonus.clear()
-            self.bonus = None
+            id = self.bonusList.pop()[0]
+            self.bonus.clearstamp(id)
+
         turtle.ontimer(self.eating, 20)
 
     def genBonus(self):
-        if self.bonus == None and self.snake.over==False:
-            print("1")
-            self.bonus = bonus()
-        turtle.ontimer(self.genBonus, 2000)
+        if len(self.bonusList) == 0 and self.state == "started":
+            self.bonusList.append(self.bonus.gen())
+        if self.state == 'started':
+            turtle.ontimer(self.genBonus, 20)
 
     def keyListening(self):
-        if(self.state == 'welcome'):
+        if(self.state == 'welcome' or self.state == 'over'):
             turtle.onkey(self.start, "g")
             turtle.listen()
         else:
